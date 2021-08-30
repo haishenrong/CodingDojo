30fe5abe195f5c7d60d05d6bc87c52515e1909cb
Lyrenyx Maxima <haishenrong@gmail.com>
2021-08-30 12:46:27 -0400

    Hello World exercise

---
+++ python/fundamentals/fundamentals/hello_world.py
@@ -0,0 +1,14 @@
+print('Hello World!')
+
+name = "Hai"
+print('Hello, {0}!'.format(name))
+print('Hello, '+name+'!')
+
+fav_num = 42
+print('Hello, {0}!'.format(fav_num))
+print('Hello, '+str(fav_num)+'!')
+
+fav_food1 = 'Durian'
+fav_food2 = 'Bitter Melon'
+print('I love to eat {0} and {1}!'.format(fav_food1, fav_food2))
+print(f'I love to eat {fav_food1} and {fav_food2}!')
