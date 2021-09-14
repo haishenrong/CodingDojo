1bb0c9755a8c01d1669723256b224eee3d95262d
Lyrenyx Maxima <haishenrong@gmail.com>
2021-09-13 20:23:51 -0400

    Login and Registration Exercise

---
+++ python/flask_mysql/validation/login/flask_app/controllers/users.py
@@ -0,0 +1,58 @@
+from flask import request, redirect, render_template, session
+from flask_app.models.user import User
+from flask_app import app
+from flask.helpers import flash
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)  
+
+@app.route("/")
+def default():
+    if session.get("user_id") == None: 
+        return render_template("index.html")
+    return redirect('/logged')
+
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": request.form['password'],
+        "cPassword": request.form['cPassword']
+    }
+    if User.validate_form(data):
+        data['password'] = bcrypt.generate_password_hash(request.form['password'])
+        user_id = User.save_user(data)
+        session['user_id'] = user_id['id']
+        return redirect('/logged')
+    return redirect('/')
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = {
+        "email": request.form['email']
+    }
+    loggedUser = User.get_one(data)
+    if not loggedUser:
+        flash("Invalid Email/Password")
+        return redirect("/")
+    if not bcrypt.check_password_hash(loggedUser.password, request.form['password']):
+        flash("Invalid Email/Password")
+        return redirect('/')
+    session['user_id'] = loggedUser.id
+    return redirect('/logged')
+
+@app.route('/logged')
+def logged():
+    data = {
+        "id": session['user_id']
+    }
+    loggedUser = User.get_by_id(data)
+    return  render_template("one_user.html", user = loggedUser)
+
+@app.route('/logout', methods = ['POST'])
+def logout():
+    session.clear()
+    return redirect('/')
