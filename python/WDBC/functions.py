97431d6f0f3485b23eaf7a077ab551d123258ff4
Lyrenyx Maxima <haishenrong@gmail.com>
2021-09-24 10:20:48 -0400

    Neural net finished

--- python/WDBC/functions.py
+++ python/WDBC/functions.py
@@ -1,10 +1,10 @@
 import math
 
 def sigmoid(x):
-    return 1/(1+math.exp(-x))
+    return 1.0/(1.0+math.exp(-x))
 
 def sigmoid_prime(x):
-    return sigmoid(x)*(1-sigmoid(x))
+    return sigmoid(x)*(1.0-sigmoid(x))
 
 def alpha_decay(x):
     return .99*x
