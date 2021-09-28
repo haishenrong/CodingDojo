6b9db0c576aca60f4790d67ea9c6e2d407c86522
Lyrenyx Maxima <haishenrong@gmail.com>
2021-09-27 20:40:25 -0400

    Exception Handling Exercise

---
+++ java/fundamentals/exceptionHandling/exceptionHandling.java
@@ -0,0 +1,22 @@
+package fundamentals.exceptionHandling;
+import java.util.*;
+
+public class exceptionHandling {
+    public static void main(String[]args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        for(int i = 0; i < myList.size(); i++) {
+            try{
+                Integer castedValue = (Integer) myList.get(i);
+            } catch (Exception e){
+                System.out.println("Error: "+ e);
+                System.out.println("Error at index " + i + ". Value of: "+myList.get(i));
+                continue;
+            };
+        }
+        System.out.println("Continued till the end.");
+    }
+}
