5e40f5284ff5dda8ffba2e8bef3d1647e9e57eb7
Lyrenyx Maxima <haishenrong@gmail.com>
2021-10-01 12:18:42 -0400

    Daikichi II path variables Exercise

--- java/springProjects/Daikichi/src/main/java/com/hsr/daikichi/DaikichiHome.java
+++ java/springProjects/Daikichi/src/main/java/com/hsr/daikichi/DaikichiHome.java
@@ -1,5 +1,6 @@
 package com.hsr.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -17,5 +18,15 @@ public class DaikichiHome {
     @RequestMapping("/tomorrow")
     public String world(){
       return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+    @RequestMapping("/travel/{city}")
+    public String travel(@PathVariable("city") String city) {
+    	return "Congratulations! You will soon travel to "+ city+"!";
+    }
+    @RequestMapping("/lotto/{num}")
+    public String lotto(@PathVariable("num") int num) {
+    	return num%2 == 0 
+    		? "You will take a grand journey in the near future, but be weary of tempting offers."
+    		: "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
     }
 }
