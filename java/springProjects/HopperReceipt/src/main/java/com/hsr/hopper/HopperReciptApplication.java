e0a12085460db957abcb3f59d813bec7a7fae004
Lyrenyx Maxima <haishenrong@gmail.com>
2021-10-01 15:21:23 -0400

    Hopper's Receipt Exercise

---
+++ java/springProjects/HopperReceipt/src/main/java/com/hsr/hopper/HopperReciptApplication.java
@@ -0,0 +1,13 @@
+package com.hsr.hopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopperReciptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopperReciptApplication.class, args);
+	}
+
+}
---
+++ java/springProjects/HopperReceipt/src/test/java/com/hsr/hopper/HopperReciptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsr.hopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopperReciptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
