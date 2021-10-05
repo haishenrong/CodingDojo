31625384e9e9e54a0f5754e125100b6d4bb8f493
Lyrenyx Maxima <haishenrong@gmail.com>
2021-10-05 16:06:06 -0400

    Books Exercise Added create

--- java/springProjects/Books/src/main/java/com/hsr/books/controllers/BookController.java
+++ java/springProjects/Books/src/main/java/com/hsr/books/controllers/BookController.java
@@ -2,10 +2,15 @@ package com.hsr.books.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.hsr.books.models.Book;
 import com.hsr.books.services.BookService;
@@ -32,4 +37,19 @@ public class BookController {
 		model.addAttribute("book", book);
 		return book == null ? "none.jsp": "show.jsp";
 	}
+	
+	@RequestMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "new.jsp";
+    }
+	
+    @RequestMapping(value="", method=RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "new.jsp";
+        } else {
+            bookService.createBook(book);
+            return "redirect:/books/";
+        }
+    }
 }
