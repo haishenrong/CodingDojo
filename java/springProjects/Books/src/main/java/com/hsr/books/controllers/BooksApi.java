8c7b0799940ffe939da53a1955afe1db21e866c6
Lyrenyx Maxima <haishenrong@gmail.com>
2021-10-05 11:18:08 -0400

    Books Exercise initial CRUD App

---
+++ java/springProjects/Books/src/main/java/com/hsr/books/controllers/BooksApi.java
@@ -0,0 +1,71 @@
+package com.hsr.books.controllers;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hsr.books.models.Book;
+import com.hsr.books.services.BookService;
+
+
+@RestController
+public class BooksApi {
+    private final BookService bookService;
+    public BooksApi(BookService bookService){
+        this.bookService = bookService;
+    }
+    @RequestMapping("/api/books")
+    @ResponseBody
+    public List<Book> index() {
+        return bookService.allBooks();
+    }
+    
+    @RequestMapping(value="/api/books", method=RequestMethod.POST)
+    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = new Book(title, desc, lang, numOfPages);
+        return bookService.createBook(book);
+    }
+    
+    @RequestMapping(value="/api/books2", method=RequestMethod.POST)
+    public Book create2(@RequestBody Map<String, Object> body) {
+        Book book = new Book((String)body.get("title"), (String) body.get("description"),(String)body.get("language"),(Integer)body.get("pages"));
+        return bookService.createBook(book);
+    }
+    
+    @RequestMapping("/api/books/{id}")
+    public Book show(@PathVariable("id") Long id) {
+        Book book = bookService.findBook(id);
+        return book;
+    }
+    
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
+    public Book update(
+    		@PathVariable("id") Long id, 
+    		@RequestParam(value="title") String title, 
+    		@RequestParam(value="description") String desc, 
+    		@RequestParam(value="language") String lang,
+    		@RequestParam(value="pages") Integer numOfPages) {
+        Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
+        return book;
+    }
+    
+    
+    @RequestMapping(value="/api/books2/{id}", method=RequestMethod.PUT)
+    public Book update2(
+    		@PathVariable("id") Long id, @RequestBody Map<String, Object> body) {
+        Book book = bookService.updateBook(id, (String)body.get("title"), (String) body.get("description"),(String)body.get("language"),(Integer)body.get("pages"));
+        return book;
+    }
+
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.DELETE)
+    public void destroy(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+    }
+}
