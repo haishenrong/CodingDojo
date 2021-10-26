b9b7accae13f56412c4111bedebc08de10358338
Lyrenyx Maxima <haishenrong@gmail.com>
2021-10-26 12:25:11 -0400

    Ninja Exercise

--- mern/OOP/Ninja/ninja.js
+++ mern/OOP/Ninja/ninja.js
@@ -8,9 +8,22 @@ class Ninja {
 
     sayName = () => console.log(this.name);
 
-    showStats = () => console.log(this.name, this.strength, this.speed, this.health);
+    showStats = () => console.log("Name:",this.name+",",
+    "Strength:", this.strength+",", "Speed:", this.speed+",",
+    "Health:", this.health);
 
     drinkSake = () => this.health += 10;
+}
+
+class Sensei extends Ninja{
+    constructor(name){
+        super(name, 210);
+        this.strength = 10;
+        this.speed = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom = () => console.log("Soon, there will be nothing.")
 }
 
 const ninja1 = new Ninja("Hyabusa");
@@ -18,3 +31,10 @@ ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
