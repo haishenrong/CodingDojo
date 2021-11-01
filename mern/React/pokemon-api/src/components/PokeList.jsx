8240767fc19044e4f5e3d657d4ec03ac99a16871
Lyrenyx Maxima <haishenrong@gmail.com>
2021-11-01 15:28:43 -0400

    pokemon-api

--- mern/React/pokemon-api/src/components/PokeList.jsx
+++ mern/React/pokemon-api/src/components/PokeList.jsx
@@ -1,15 +1,20 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
 const PokeList = () => {
     const [pokemon, setPokemon] = useState([]);
 
     const fetchPoke = async () => {
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=807')
+            .then(response=>response.data)
+            .then(response=>setPokemon(response.results))
+        /*
         let response = await fetch(
             "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=807")
             .then(res => res.json());
         const pokeList = response.results;
         setPokemon(pokeList);
-        //return response.json();
+        */
     }
 
 
