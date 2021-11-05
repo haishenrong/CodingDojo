a1875262c27d4fc4e020fd71b1a391e8d29ed18e
Lyrenyx Maxima <haishenrong@gmail.com>
2021-11-05 14:41:38 -0400

    Jokes api

---
+++ mern/jokes-api/server/controllers/joke.controller.js
@@ -0,0 +1,31 @@
+const Joke = require("../models/joke.model");
+
+module.exports.findAllJokes = (req, res) => {
+    Joke.find()
+    .then(jokes => res.json({ jokes: jokes}))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.findJoke = (req, res) => {
+	Joke.findOne({ _id: req.params.id })
+		.then(joke => res.json({ joke: joke }))
+		.catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.createJoke = (req, res) => {
+    Joke.create(req.body)
+    .then(newJoke => res.json({ joke: newJoke }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.updateJoke = (req, res) => {
+    Joke.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(updatedJoke => res.json({ joke: updatedJoke }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+module.exports.deleteJoke = (req, res) => {
+    Joke.deleteOne({ _id: req.params.id })
+    .then(result => res.json({ result: result }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
